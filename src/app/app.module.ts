b021044328fb4451a66d4874f1a8a4fb657d0ac2
Artemas <artemasseng@gmail.com>
2017-06-26 00:34:12 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {BasketComponent} from '../app/basket/basket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasketComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    BasketComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/basket/item.ts
@@ -0,0 +1,10 @@
+export class Item {
+
+  constructor(
+    public id: String,
+    public name: String,
+    public description: String,
+    public category: String
+  ) { }
+
+}
